4d926c9dad8472509b6da778528085ffe0df64e4
David Darx <daviddarx@gmail.com>
2019-06-26 19:54:48 +0200

    debug page transition delay due to parseInt

--- interface/javascript/general.js
+++ interface/javascript/general.js
@@ -1,11 +1,5 @@
 /*
-
-- check problem scale filter -> comparer avec daviddarx.cim
-		- check screenshot à la racine. 
-		- check sur anime 01 aussi. 
-				- surement le ratio. 
-		- check sur ecrans pas retina? 
-
+ 
 - faire backup de version actuellement sur daviddarx pour comparer perfs
 -- check sur daviddar.com si le landing sur index.html fonctionne. 
 -- check dist sur daviddarx si probleme de load listener des images pour scene1, commne sur netlify
@@ -118,8 +112,7 @@ const Global = function(){
 		this.domRefs.$paginationItems[this.stageID-1].classList.add('active');
 		
 		this.stageAnimationOutDuration = window.getComputedStyle ? getComputedStyle(this.domRefs.$stageContainer, null) : this.domRefs.$stageContainer.currentStyle;
-		this.stageAnimationOutDuration = parseInt(this.stageAnimationOutDuration.transitionDuration.split('s')[0])*1000;
-		
+		this.stageAnimationOutDuration = parseFloat(this.stageAnimationOutDuration.transitionDuration.split('s')[0])*1000;
 		window.addEventListener('resize', this.resizeListener);
 		this.resizeListener();
 		
